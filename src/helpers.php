e0acca83add53344bb4199e1eae1857e813a1309
Ciaran Downey <me@ciarand.me>
2014-02-05 17:59:04 -0800

    Add helpers file, "…" (yada yada yada) function

---
+++ src/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * A collection of (arguably) useful helpers
+ */
+
+/**
+ * The "…" method functions similarly to Perl's "..." operator (the "yada, 
+ * yada, yada" operator) and dies if it's ever called. It's used as a stub for
+ * unwritten code.
+ *
+ * @inspiration Perl6
+ */
+function …()
+{
+    die("Method stub");
+}
