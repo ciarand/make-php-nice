a424c36efc655186cc7696f47e41d9f7a30ab933
Ciaran Downey <me@ciarand.me>
2014-07-12 16:50:40 -0700

    Add stolen functions from nikic/iter

--- src/helpers.php
+++ src/helpers.php
@@ -15,3 +15,162 @@ function …()
 {
     die("Method stub");
 }
+
+/**
+ * All taken shamelessly from @nikic's fantastic iter library. The only reason
+ * they're in here as opposed to just pulling in the main library is that it
+ * has a 5.5 dependency and I don't get to use 5.5 at work yet. Also namespaces.
+ *
+ * Source: https://github.com/nikic/iter
+ */
+
+/**
+ * Returns a callable which returns an item from an array corresponding to the
+ * given index
+ *
+ * @param mixed index
+ *
+ * @return callable
+ */
+function index($index)
+{
+    return function($array) use ($index) {
+        return $array[$index];
+    };
+}
+
+/**
+ * Returns a callable which returns an item from a nested array corresponding
+ * to the given path.
+ *
+ * Examples:
+ *
+ *     $array = [
+ *         'foo' => [
+ *             'bar' => [
+ *                 'baz' => 42
+ *             ]
+ *         ]
+ *     ];
+ *
+ *     $getIndexFooBar = fn\nested_index('foo', 'bar');
+ *     $getIndexFooBarBaz = fn\nested_index('foo', 'bar', 'baz');
+ *
+ *     $getIndexFooBar($array)
+ *     => ['baz' => 42]
+ *
+ *     $getIndexFooBarBaz($array)
+ *     => 42
+ *
+ * @param mixed[] ...$indices Path of indices
+ *
+ * @return callable
+ */
+function nested_index(/* ...$indices */) {
+    $indices = func_get_args();
+
+    return function($array) use ($indices) {
+        foreach ($indices as $index) {
+            $array = $array[$index];
+        }
+
+        return $array;
+    };
+}
+
+/**
+ * Returns a callable which returns a property from an object corresponding to
+ * the provided string
+ *
+ * @param string $propertyName
+ *
+ * @return callable
+ */
+function property($propertyName) {
+    return function($object) use ($propertyName) {
+        return $object->$propertyName;
+    };
+}
+
+/**
+ * Returns a callable which returns the result of a method call corresponding
+ * to the provided string and args
+ *
+ * @param string $propertyName
+ * @param array $args
+ *
+ * @return callable
+ */
+function method($methodName, array $args = []) {
+    if (empty($args)) {
+        return function($object) use ($methodName) {
+            return $object->$methodName();
+        };
+    } else {
+        return function($object) use ($methodName, $args) {
+            return call_user_func_array([$object, $methodName], $args);
+        };
+    }
+}
+
+/**
+ * Returns a callable which returns the result of the provided operator on its
+ * two arguments.
+ *
+ * @param string $operator
+ * @param mixed $arg
+ *
+ * @return callable
+ */
+function operator($operator, $arg = null) {
+    $functions = [
+        'instanceof' => function($a, $b) { return $a instanceof $b; },
+        '*'   => function($a, $b) { return $a *   $b; },
+        '/'   => function($a, $b) { return $a /   $b; },
+        '%'   => function($a, $b) { return $a %   $b; },
+        '+'   => function($a, $b) { return $a +   $b; },
+        '-'   => function($a, $b) { return $a -   $b; },
+        '.'   => function($a, $b) { return $a .   $b; },
+        '<<'  => function($a, $b) { return $a <<  $b; },
+        '>>'  => function($a, $b) { return $a >>  $b; },
+        '<'   => function($a, $b) { return $a <   $b; },
+        '<='  => function($a, $b) { return $a <=  $b; },
+        '>'   => function($a, $b) { return $a >   $b; },
+        '>='  => function($a, $b) { return $a >=  $b; },
+        '=='  => function($a, $b) { return $a ==  $b; },
+        '!='  => function($a, $b) { return $a !=  $b; },
+        '===' => function($a, $b) { return $a === $b; },
+        '!==' => function($a, $b) { return $a !== $b; },
+        '&'   => function($a, $b) { return $a &   $b; },
+        '^'   => function($a, $b) { return $a ^   $b; },
+        '|'   => function($a, $b) { return $a |   $b; },
+        '&&'  => function($a, $b) { return $a &&  $b; },
+        '||'  => function($a, $b) { return $a ||  $b; },
+    ];
+
+    if (!isset($functions[$operator])) {
+        throw new \InvalidArgumentException("Unknown operator \"$operator\"");
+    }
+
+    $fn = $functions[$operator];
+    if (func_num_args() === 1) {
+        return $fn;
+    } else {
+        return function($a) use ($fn, $arg) {
+            return $fn($a, $arg);
+        };
+    }
+}
+
+/**
+ * Returns a callable that returns the inverse (!) of the provided callable
+ *
+ * @param callable $function
+ *
+ * @return callable
+ */
+function not($function) {
+    return function() use ($function) {
+        return !call_user_func_array($function, func_get_args());
+    };
+}
